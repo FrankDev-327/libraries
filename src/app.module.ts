7e289833a7fc346501c1220eba10f710b7e4b4c0
FrankDev-327 <renteriafrancisco51@gmail.com>
2023-08-09 14:30:37 +0200

    added users and books modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,23 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HealthChecAppkModule } from './health-check/health-check.module';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { BooksController } from './books/books.controller';
+import { BooksService } from './books/books.service';
+import { BooksModule } from './books/books.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeormConfig),
-    HealthChecAppkModule
+    HealthChecAppkModule,
+    UsersModule,
+    BooksModule
   ],
+  providers: [UsersService, BooksService],
+  controllers: [UsersController, BooksController],
 })
 export class AppModule {}
