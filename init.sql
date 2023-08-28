e148329e785b020755f8506b1bd59f66b9680a64
FrankDev-327 <renteriafrancisco51@gmail.com>
2023-08-29 01:06:42 +0200

    modified init.sql

--- init.sql
+++ init.sql
@@ -2,11 +2,24 @@ SELECT * FROM pg_extension WHERE extname = 'pgcrypto';
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-INSERT INTO users (name, "lastName", email, password, role)
-VALUES (
-    'admin_user', 
-    'admin_user', 
-    'admin_user@gmail.com',
-    crypt('123456789', gen_salt('bf')), 
-    'ADMIN'
-);
+DO
+$$
+DECLARE
+    lcount integer;
+
+BEGIN    
+    SELECT COUNT(*) FROM users 
+    WHERE email = 'admin_user@gmail.com';
+
+    IF (lcount <= 0) then
+        INSERT INTO users (name, "lastName", email, password, role)
+        VALUES (
+        'admin_user', 
+        'admin_user', 
+        'admin_user@gmail.com',
+        crypt('123456789', gen_salt('bf')), 
+        'ADMIN'
+        );
+    END IF;
+END;
+$$
